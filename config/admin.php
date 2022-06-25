bc2052e579e13429c1059525059736a696688247
xutongle <xutongle@gmail.com>
2022-06-25 21:59:08 +0800

    初始导入

---
+++ config/admin.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * This is NOT a freeware, use is subject to license terms.
+ *
+ * @copyright Copyright (c) 2022-2099 Jinan Larva Information Technology Co., Ltd.
+ */
+
+return [
+
+];
---
+++ src/Facades/Admin.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * This is NOT a freeware, use is subject to license terms.
+ *
+ * @copyright Copyright (c) 2010-2099 Jinan Larva Information Technology Co., Ltd.
+ */
+
+namespace Larva\Admin\Facades;
+
+class Admin
+{
+}
