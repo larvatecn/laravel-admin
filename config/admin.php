0d447f043fae44ab75ce144bd9ddd7d2296fe39a
XuTongle <xutongle@msn.com>
2022-06-29 13:21:33 +0800

    更新

--- config/admin.php
+++ config/admin.php
@@ -5,6 +5,11 @@
  * @copyright Copyright (c) 2022-2099 Jinan Larva Information Technology Co., Ltd.
  */
 
+use Larva\Admin\Models\AdminUser;
+use Larva\Admin\Models\Menu;
+use Larva\Admin\Models\Permission;
+use Larva\Admin\Models\Role;
+
 return [
     'name' => 'Admin',
 
@@ -19,4 +24,23 @@ return [
     'directory' => app_path('Admin'),
 
     'https' => env('ADMIN_HTTPS', false),
+
+    'database' => [
+        'connection' => '',
+        'users_table' => 'admin_users',
+        'users_model' => AdminUser::class,
+        'roles_table' => 'admin_roles',
+        'roles_model' => Role::class,
+        'permissions_table' => 'admin_permissions',
+        'permissions_model' => Permission::class,
+        'menu_table' => 'admin_menu',
+        'menu_model' => Menu::class,
+
+        'user_permissions_table' => 'admin_user_permissions',
+        'role_users_table' => 'admin_role_users',
+        'permission_menu_table' => 'admin_permission_menu',
+        'role_permissions_table' => 'admin_role_permissions',
+        'role_menu_table' => 'admin_role_menu',
+        'settings_table' => 'admin_settings',
+    ],
 ];
