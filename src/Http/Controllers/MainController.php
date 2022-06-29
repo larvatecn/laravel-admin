02a5ed8885acacda008cac22fa1479801efd6b5b
xutongle <xutongle@gmail.com>
2022-06-29 23:21:22 +0800

    update

---
+++ src/Http/Controllers/MainController.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * This is NOT a freeware, use is subject to license terms.
+ *
+ * @copyright Copyright (c) 2022-2099 Jinan Larva Information Technology Co., Ltd.
+ */
+
+namespace Larva\Admin\Http\Controllers;
+
+class MainController extends AdminController
+{
+
+    public function index()
+    {
+        $config = [
+            'name' => config('admin.name'),
+            'title' => config('admin.title'),
+            'apiBase' => admin_url(config('admin.route.prefix')),
+            'prefix' => config('admin.route.prefix'),
+            'loginLogo' => config('admin.loginLogo'),
+            'loginDesc' => config('admin.loginDesc'),
+            'copyright' => config('admin.copyright'),
+            'footerLinks' => config('admin.footerLinks'),
+        ];
+
+        return view('admin::main.index', ['config' => $config]);
+    }
+}
--- src/routes.php
+++ src/routes.php
@@ -4,3 +4,40 @@
  *
  * @copyright Copyright (c) 2010-2099 Jinan Larva Information Technology Co., Ltd.
  */
+
+use Illuminate\Routing\Router;
+use Illuminate\Support\Facades\Route;
+use Larva\Admin\Http\Controllers\AdminUserController;
+use Larva\Admin\Http\Controllers\AuthController;
+use Larva\Admin\Http\Controllers\HandleController;
+use Larva\Admin\Http\Controllers\MainController;
+use Larva\Admin\Http\Controllers\MenuController;
+use Larva\Admin\Http\Controllers\PermissionController;
+use Larva\Admin\Http\Controllers\RoleController;
+
+Route::group([
+    'domain' => config('admin.route.domain'),
+    'prefix' => config('admin.route.prefix', 'admin'),
+    'middleware' => config('admin.route.middleware'),
+], static function (Router $router) {
+    $router->get('/', [MainController::class, "index"]);
+    $router->get('view', [MainController::class, "index"]);
+    $router->get('view/{name}', [MainController::class, "index"])->where('name', '.*');
+    $router->get('getMenu', [HandleController::class, "menu"])->name('admin.getMenu');
+    $router->get('getHeaderToolbar', [HandleController::class, 'headerToolbar'])->name('admin.headerToolbar');
+    $router->any('_handle_action_', [HandleController::class, 'action'])->name('admin.handle-action');
+    $router->post('_handle_upload_image_', [HandleController::class, 'uploadImage'])->name('admin.handle-upload-image');
+    $router->post('_handle_upload_file_', [HandleController::class, 'uploadFile'])->name('admin.handle-upload-file');
+
+    $authController = config('admin.auth.controller', AuthController::class);
+
+    $router->resource('login', $authController)->names('admin.login')->only(['index', 'store']);
+    $router->get('logout', [$authController, 'logout'])->name('admin.logout');
+    $router->any('user_setting', [$authController, 'userSetting'])->name('admin.userSetting');
+
+    $router->resource('admin_users', AdminUserController::class)->names('admin.user');
+    $router->resource('menus', MenuController::class)->names('admin.menu');
+    $router->resource('roles', RoleController::class)->names('admin.role');
+    $router->resource('permissions', PermissionController::class)->names('admin.permission');
+    $router->get('permissions_auto_generate', [PermissionController::class, 'autoGenerate'])->name('admin.permission.auto-generate');
+});
